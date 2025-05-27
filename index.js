708277b4c1406ab8a3dcda6a78730ef04e261534
Yoda2025 <jan@kody17.com>
2025-05-27 10:57:51 +0200

    První čistý commit backendu

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/send-email', async (req, res) => {
+  const {
+    name,
+    phone,
+    date,
+    time,
+    pickup,
+    destination,
+    passengers,
+    luggageHand,
+    luggageSmall,
+    luggageLarge
+  } = req.body;
+
+  const templateParams = {
+    name,
+    phone,
+    date,
+    time,
+    pickup,
+    destination,
+    passengers,
+    luggageHand,
+    luggageSmall,
+    luggageLarge
+  };
+
+  try {
+    const response = await axios.post('https://api.emailjs.com/api/v1.0/email/send', {
+      service_id: 'service_mj8y5tl',
+      template_id: 'template_j6409i3',
+      user_id: 'a2FHdsm7E6XnY808PNSo2',
+      template_params: templateParams
+    });
+
+    res.status(200).json({ message: 'Email odeslán' });
+  } catch (error) {
+    console.error('Chyba při odesílání e-mailu:', error.response?.data || error.message);
+    res.status(500).json({ message: 'Chyba při odesílání e-mailu' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server běží na portu ${PORT}`);
+});
